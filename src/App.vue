60cf44ec25eacebe36996fbadf008bdd958afacc
SHUO <iwilliam147258@gmail.com>
2024-02-20 00:10:43 +0800

    引入BS5 修改變數設定(間距、背景色、字體大小 新增顏色變數、字型

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,6 @@
 export default {}
 </script>
 
-<style lang="scss">
+<!-- <style lang="scss">
 
-</style>
+</style> -->
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './assets/main.css'
-
+// import './assets/main.css'
+import './assets/all.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
