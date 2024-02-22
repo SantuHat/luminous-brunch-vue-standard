2fd225fba1e3288064ac6178dbb46e20569d0d0d
SHUO <iwilliam147258@gmail.com>
2024-02-22 23:02:36 +0800

    新增env 以及axios loading

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 import './assets/all.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Loading from 'vue3-loading-overlay'
+import 'vue3-loading-overlay/dist/vue3-loading-overlay.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('LoadingView', Loading)
+app.use(VueAxios, axios)
 app.mount('#app')
