c3205f1331eae3b5c300e4a50fbd8a6a2df13436
Santu <j6458221@yahoo.com.tw>
2024-03-03 23:54:03 +0800

    點擊購物車顯示浮框，吐司顯示加入購物車成功

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -43,16 +43,40 @@
             </li>
           </ul>
         </li>
-        <RouterLink to="/usercart">
-          <li class="nav_item border-bottom-1">
-            <a class="py-4 py-lg-3 px-5 fw-bold d-block d-flex" href="#">
+        <!-- <RouterLink to="/usercart"> -->
+          <!-- Bootstrap Dropdowns -->
+          <li @click="getCarts()" class="nav_item border-bottom-1" id="cartFrame" data-bs-toggle="dropdown" aria-expanded="false" ref="cartFrame">
+            <a class="py-4 py-lg-3 px-5 fw-bold d-block d-flex" href="javascript:;">
               <span class="material-symbols-outlined me-1">
                 shopping_cart
               </span>
               <span>購物車</span>
             </a>
           </li>
-        </RouterLink>
+          <div class="dropdown-menu bg-pink" aria-labelledby="cartFrame"  @click.stop>
+            <div class="dropdown-menu-content">
+              <ul v-for="(item) in carts" :key="item.id" class="list-unstyled">
+                <div class="dropdown-item" href="#">
+                  <img :src="item.product.imageUrl"  class="rounded me-2 toast-img" alt="...">
+                      {{ item.product.title }}
+                      X {{ item.qty }}
+                      ${{ item.product.price }}
+                  <button type="button" class="btn btn-primary" @click="delCart(item.id)">
+                      <span class="material-symbols-outlined pt-1">
+                          delete
+                      </span>
+                  </button>
+                </div>
+              </ul>
+            </div>
+            <div class="dropdown-menu-footer text-center">
+              <RouterLink to="/usercart">
+                <button type="button" class="btn btn-primary py-3 px-5 mt-3">前往結帳</button>
+              </RouterLink>
+            </div>
+          </div>
+
+        <!-- </RouterLink> -->
         <li v-if="isLogin" class="memberCenterItem">
           <a @click="dropdownMemberCenter" class="selectMemberCenter py-4 px-5 fw-bold position-relative d-block border-bottom-1 d-flex justify-content-between"
             href="javascript:;">會員中心<span class="material-symbols-outlined align-bottom">
@@ -100,6 +124,9 @@
 import { mapActions, mapState } from 'pinia'
 import counterStore from '@/stores/counter'
 import { RouterLink } from 'vue-router'
+import { Dropdown } from 'bootstrap'
+
+const { VITE_API, VITE_PATH } = import.meta.env
 
 export default {
   name: 'HeaderView',
@@ -107,7 +134,9 @@ export default {
     return {
       isActive: false,
       isMemberActive: false,
-      isLogin: false
+      isLogin: false,
+      cartFrame: '',
+      carts: []
     }
   },
   methods: {
@@ -121,22 +150,71 @@ export default {
     logOut () {
       localStorage.setItem('isLogin', false)
       this.isLogin = false
+      console.log(this.isLogin)
       alert('已登出')
+    },
+    getCarts () {
+      const url = `${VITE_API}/api/${VITE_PATH}/cart`
+      this.isLoading = true
+      this.$http.get(url)
+        .then((res) => {
+          const { carts } = res.data.data
+          this.carts = carts
+          this.isLoading = false
+          console.log(carts)
+        })
+        .catch((err) => {
+          console.log(err)
+          this.isLoading = false
+        })
+    },
+    delCart (id) {
+      const url = `${VITE_API}/api/${VITE_PATH}/cart/${id}`
+      this.isLoading = true
+      this.$http.delete(url)
+        .then(() => {
+          this.isLoading = false
+          this.getCarts()
+        })
+        .catch((err) => {
+          console.log(err)
+          this.isLoading = false
+        })
     }
   },
   computed: {
     ...mapState(counterStore, ['count'])
   },
+  created () {
+    this.getCarts()
+  },
   mounted () {
     const isLogin = localStorage.getItem('isLogin')
-    // console.log(isLogin, 'localSto')
     this.isLogin = isLogin === 'true'
-    // console.log(this.isLogin)
+    // console.log('this.$refs.cartFrame', this.$refs.cartFrame)
+    this.cartFrame = new Dropdown(this.$refs.cartFrame)
+    // console.log(' this.cartFrame', this.cartFrame)
+  },
+  watch: {
+    isLogin (val) {
+      console.log('val', val)
+    }
   },
   components: { RouterLink }
 }
 </script>
 
 <style scoped>
-
+.dropdown-menu {
+  width: 100%;
+  max-width: 500px;
+  box-shadow: 0 0.5rem 1rem rgba(0,0,0,.15);
+}
+.dropdown-menu-content {
+  height: 320px;
+  overflow-y: auto;
+}
+.dropdown-menu-content::-webkit-scrollbar {
+  display: none;
+}
 </style>
