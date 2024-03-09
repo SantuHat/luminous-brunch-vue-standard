f389b7fd3468bab17a225f66326496f56d28976d
SHUO <iwilliam147258@gmail.com>
2024-03-09 16:46:40 +0800

    加入 deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录下
+cd dist
+
+# 部署到自定义域域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/SantuHat/luminous-brunch-vue-standard.git master:gh-pages
+
+cd -
