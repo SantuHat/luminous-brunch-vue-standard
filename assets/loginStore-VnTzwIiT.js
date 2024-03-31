2bd91ec73af45c3e23a0ab0db27ad0a3d40b3337
Santu <j6458221@yahoo.com.tw>
2024-03-31 23:44:42 +0800

    Updates

--- assets/loginStore-VnTzwIiT.js
+++
@@ -1,1 +0,0 @@
-import{u as i}from"./index-DB9Op6xa.js";const s=i("loginStore",{state:()=>({isLogin:!1}),actions:{setLogin(o){localStorage.setItem("isLogin",o),this.isLogin=o},getLogin(){const o=localStorage.getItem("isLogin");this.isLogin=o==="true"}}});export{s as l};
