7a43682438ea9a0a773fbb6a38f39393a7ff8d60
santu <j6458221@yahoo.com.tw>
2024-04-01 15:08:02 +0800

    Updates

--- assets/loginStore-CxBS7Z5F.js
+++
@@ -1,1 +0,0 @@
-import{u as i}from"./index-BkM3Jw5p.js";const s=i("loginStore",{state:()=>({isLogin:!1}),actions:{setLogin(o){localStorage.setItem("isLogin",o),this.isLogin=o},getLogin(){const o=localStorage.getItem("isLogin");this.isLogin=o==="true"}}});export{s as l};
---
+++ assets/loginStore-wXI-d9cN.js
@@ -0,0 +1,1 @@
+import{u as i}from"./index-b4dgHHPy.js";const s=i("loginStore",{state:()=>({isLogin:!1}),actions:{setLogin(o){localStorage.setItem("isLogin",o),this.isLogin=o},getLogin(){const o=localStorage.getItem("isLogin");this.isLogin=o==="true"}}});export{s as l};
