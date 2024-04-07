8dac36f04f8c73eba37688860c6aec970db4fe59
Santu <j6458221@yahoo.com.tw>
2024-04-08 00:08:57 +0800

    Updates

---
+++ assets/loginStore-LaYuIcQl.js
@@ -0,0 +1,1 @@
+import{y as i}from"./index-CPGaNytM.js";const s=i("loginStore",{state:()=>({isLogin:!1}),actions:{setLogin(o){localStorage.setItem("isLogin",o),this.isLogin=o},getLogin(){const o=localStorage.getItem("isLogin");this.isLogin=o==="true"}}});export{s as l};
